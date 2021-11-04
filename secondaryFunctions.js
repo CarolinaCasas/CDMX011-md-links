907e0b61facbbcff7237e542bab756e73a912cea
CarolinaCasas <casasc9@uabc.edu.mx>
2021-11-03 22:32:14 -0700

    allPathFilles

--- secondaryFunctions.js
+++ secondaryFunctions.js
@@ -8,28 +8,48 @@ const mdFile = (absoluteRoute) => {
 
 
 const extractMd = (absoluteRoute) => {
-    
-    fs.readdir(absoluteRoute, (error, files) => {
-        const allFiles=[];
+     fs.readdir(absoluteRoute, (error, files) => {
         if (error) {
-            return allFiles.push(error);
+            console.log(error);
         } else{
-           return allFiles.concat(files);
-        }
+
+          const filePath =  files.map((filesNeedPath)=> absoluteRoute +'/'+filesNeedPath);
+          /* filePath.forEach((allFilePath)=>{
+              if(fs.lstatSync(allFilePath).isDirectory()===true){
+    
+            console.log(extractMd(filePath))}
+        }); */
+console.log(filePath);
         
-        // recupera todos los archivos
-        
-        // filtra los archivos con extención md
-       // const filesFilter = files.filter(allFiles => path.extname(allFiles) == '.md');
-       //return allFiles;
-       // console.log(filesFilter);
-    });
+    };
+    
 
-};
+});
 
-
+}
 
 module.exports = {
     mdFile,
     extractMd
 }
+
+// filtra los archivos con extención md
+       // const filesFilter = files.filter(allFiles => path.extname(allFiles) == '.md');
+       //return allFiles;
+       // console.log(filesFilter);
+
+
+/* for (let index = 0; index < files.length; ++index) { 
+    let file = files[index]; 
+    if (file[0] !== '.') { 
+        let filePath = rootDir + '/' + file; 
+        fs.stat(filePath, function(err, stat) {
+            if (stat.isDirectory()) { 
+                dirs.Push(this.file); 
+            } 
+            if (files.length === (this.index + 1)) { 
+                return cb(dirs); 
+            } 
+        }.bind({index: index, file: file})); 
+    }
+} */
