40e26e4f79004887dcffa24a2d07e90f733e580e
CarolinaCasas <casasc9@uabc.edu.mx>
2021-11-04 23:33:11 -0700

    thirdRefactoring

--- getPathFunction.js
+++ getPathFunction.js
@@ -1,60 +1,38 @@
-const secondaryFunctions = require('./secondaryFunctions.js');
-
-/* 
-const getPath = ( entrypath )=> {
 const fs = require('fs');
-const pathResolve = require('path').resolve
 const path = require('path')
-const absoluteRoute = pathResolve(entrypath);
-console.log(absoluteRoute)
 
-// is a file
+const getPath = (entrypath, callbackpath=[]) => {
 
-
-if(fs.lstatSync(absoluteRoute).isDirectory()===true){
-  fs.readdir(absoluteRoute, (error, files) => {
-    if (error) {
-    console.log(error);
-    }
-    // recupera todos los archivos
-    console.log(files);
-    //filtra los archivos con extención md
-    const filesMD = files.filter(allFiles=> path.extname(allFiles) == '.md')
-    console.log(filesMD);
-    });  
-}
-else{
-  path.extname(absoluteRoute) == '.md'? console.log(fs.readFileSync(absoluteRoute, 'utf8')): console.log('No es un archivo md');
-
-}
-
- 
-//saca los archivos md
-
-} */
-
-
-
-const getPath = ( entrypath )=> {
-  const fs = require('fs');
-  const pathResolve = require('path').resolve
-  const absoluteRoute = pathResolve(entrypath);
+  const absoluteRoute = path.resolve(entrypath);
   
-// is directory
+  if (fs.lstatSync(absoluteRoute).isDirectory() === true) {
+    fs.readdir(absoluteRoute, (error, files) => {
+      if (error) {
+        console.log(error);
+      }
 
-  if(fs.lstatSync(absoluteRoute).isDirectory()===true){
+      //retorna todas las rutas absolutas dentro del array
+     const filePath = files.map((filesNeedPath) => absoluteRoute + '/' + filesNeedPath);
     
-      return secondaryFunctions.extractMd(absoluteRoute);
-  
-  }else{
-      return secondaryFunctions.mdFile(absoluteRoute);
-  }
-   
-  
-  }
+     //Retorna las 
+      //detectar si hay mas carpetas
+     const file = filePath.map((allfilePath) => {
+      getPath(allfilePath, callbackpath=[])
+      });
 
+      console.log(file);
 
+      
+    });   
+    //console.log(filePath);
+}
+//else{
+//const filesMD = filePath.filter(allFiles => path.extname(allFiles) == '.md')
+//return callbackpath.push(filesMD)+'owo';
+//}
 
-module.exports ={
+}
+
+module.exports = {
   getPath
 }
